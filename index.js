6ee0400a0ad27114121571ad3772cd1afc3c8438
marziamostafa <marziamostafa@gmail.com>
2022-11-09 18:01:48 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 // const jwt = require('jsonwebtoken')
 
@@ -10,6 +10,12 @@ const port = process.env.PORT || 5000;
 //middle wares
 app.use(cors());
 app.use(express.json())
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.kkoxxt5.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send('foodie server site running')
